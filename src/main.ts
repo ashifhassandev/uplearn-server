d706d62f75684e3d79551ce8ec6f1032876294dd
Ashif Hassan <ashifhassan1996@gmail.com>
2026-01-24 10:58:07 +0530

    style: fix indentation in main.ts

--- src/main.ts
+++ src/main.ts
@@ -4,16 +4,14 @@ import { connectMongoDB } from "./infrastructure/database/mongoose/connection";
 import { createExpressApp } from "./infrastructure/web/express-app";
 
 const bootstrap = async () => {
-	await connectMongoDB();
+  await connectMongoDB();
 
-	const app = createExpressApp();
+  const app = createExpressApp();
 
-	const BASE_URL = process.env.BASE_URL
-	const PORT = process.env.PORT || 3000;
+  const BASE_URL = process.env.BASE_URL;
+  const PORT = process.env.PORT || 3000;
 
-	app.listen(PORT, () => {
-		console.log(`Server running on ${BASE_URL}:${PORT}`);
-	});
+  app.listen(PORT, () => console.log(`Server running on ${BASE_URL}:${PORT}`));
 };
 
 bootstrap();
