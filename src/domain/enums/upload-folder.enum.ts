af7356073cd4ea0497edd8a1eeb2a16cb901a39a
Ashif Hassan <ashifhassan1996@gmail.com>
2026-09-21 12:13:42 +0530

    feat(domain): add domain enums

---
+++ src/domain/enums/upload-folder.enum.ts
@@ -0,0 +1,6 @@
+export enum UploadFolder {
+  PROFILE_IMAGES = "profile-images",
+  TUTOR_DOCUMENTS = "tutor-documents",
+  COURSE_THUMBNAILS = "course-thumbnails",
+  COURSE_VIDEOS = "course-videos",
+}
