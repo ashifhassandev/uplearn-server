a3120169c3e396c67420487911023a12dfc56130
Ashif Hassan <ashifhassan1996@gmail.com>
2026-09-21 20:12:51 +0530

    feat(database): add student details repository

---
+++ src/infrastructure/repositories/student-details.repository.impl.ts
@@ -0,0 +1,25 @@
+import { injectable } from "tsyringe";
+
+import { IStudentDetailsRepository } from "../../application/ports/repositories/student-details.repository.interface";
+import { StudentDetails } from "../../domain/entities/student-details.entity";
+import { StudentDetailsModel } from "../database/models/student-details.model";
+import { StudentDetailsMapper } from "../mappers/student-details.mapper";
+
+@injectable()
+export class StudentDetailsRepositoryImpl implements IStudentDetailsRepository {
+  async create(userId: string): Promise<StudentDetails> {
+    const doc = await StudentDetailsModel.create({ userId });
+    return StudentDetailsMapper.toDomain(doc);
+  }
+
+  async findByUserId(userId: string): Promise<StudentDetails | null> {
+    const doc = await StudentDetailsModel.findOne({ userId });
+    if (!doc) return null;
+    
+    return StudentDetailsMapper.toDomain(doc);
+  }
+
+  async deleteByUserId(userId: string): Promise<void> {
+    await StudentDetailsModel.findOneAndDelete({ userId });
+  }
+}
