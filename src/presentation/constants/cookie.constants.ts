312f2ace156fc44d628727f22419836f33f013fd
Ashif Hassan <ashifhassan1996@gmail.com>
2026-09-22 09:56:36 +0530

    feat(presentation): add cookie constants

---
+++ src/presentation/constants/cookie.constants.ts
@@ -0,0 +1,14 @@
+export const COOKIE_NAMES = {
+  REFRESH_TOKEN: "refreshToken",
+  ACCESS_TOKEN: "accessToken",
+} as const;
+
+export const COOKIE_OPTIONS = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite:
+    process.env.NODE_ENV === "production"
+      ? ("strict" as const)
+      : ("lax" as const),
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+} as const;
